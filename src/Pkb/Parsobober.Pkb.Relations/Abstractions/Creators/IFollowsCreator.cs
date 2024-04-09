e101cbee0daa136110ab6f7bd0684d2b1107d81f
pzajaczkowski <demoon.pps@gmail.com>
2024-04-09 19:42:27 +0200

    [BOB-14] Add relations creators abstractions

---
+++ src/Pkb/Parsobober.Pkb.Relations/Abstractions/Creators/IFollowsCreator.cs
@@ -0,0 +1,16 @@
+﻿using Parsobober.Pkb.Ast;
+
+namespace Parsobober.Pkb.Relations.Abstractions.Creators;
+
+/// <summary>
+/// Interface for creating a follows relationship between two nodes in a tree.
+/// </summary>
+public interface IFollowsCreator
+{
+    /// <summary>
+    /// Sets the follows relationship between the specified node and its parent.
+    /// </summary>
+    /// <param name="node">The node to establish the follows relationship with.</param>
+    /// <param name="parent">The parent node of the specified node.</param>
+    void SetFollows(TreeNode node, TreeNode parent);
+}
---
+++ src/Pkb/Parsobober.Pkb.Relations/Abstractions/Creators/IParentCreator.cs
@@ -0,0 +1,16 @@
+﻿using Parsobober.Pkb.Ast;
+
+namespace Parsobober.Pkb.Relations.Abstractions.Creators;
+
+/// <summary>
+/// Interface for creating parent relation pairs in pkb.
+/// </summary>
+public interface IParentCreator
+{
+    /// <summary>
+    /// Sets the parent of a child node.
+    /// </summary>
+    /// <param name="parentNode">The parent node to which the child node will be attached.</param>
+    /// <param name="childNode">The child node that will be attached to the parent node.</param>
+    void SetParent(TreeNode parentNode, TreeNode childNode);
+}
