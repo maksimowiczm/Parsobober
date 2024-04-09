46d122cc995b5214eda84b4aa8d828181232dfa1
pzajaczkowski <demoon.pps@gmail.com>
2024-04-09 19:30:00 +0200

    [BOB-14] Add relations accessors abstractions

---
+++ src/Pkb/Parsobober.Pkb.Relations/Abstractions/IPkbAccessor.cs
@@ -0,0 +1,11 @@
+﻿using Parsobober.Pkb.Relations.Abstractions.Accessors;
+
+namespace Parsobober.Pkb.Relations.Abstractions;
+
+public interface IPkbAccessor
+{
+    IFollowsAccessor Follows { get; }
+    IParentAccessor Parent { get; }
+    IModifiesAccessor Modifies { get; }
+    IUsesAccessor Uses { get; }
+}
