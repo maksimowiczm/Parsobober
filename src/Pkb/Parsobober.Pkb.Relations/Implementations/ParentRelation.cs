cd5e060e97f7b52b27968f4e3e28d5e451c670fc
pzajaczkowski <demoon.pps@gmail.com>
2024-04-16 13:23:08 +0200

    [BOB-17] Fix ParentCreator implementation, adjust type checks.

--- src/Pkb/Parsobober.Pkb.Relations/Implementations/ParentRelation.cs
+++ src/Pkb/Parsobober.Pkb.Relations/Implementations/ParentRelation.cs
@@ -12,23 +12,34 @@ public class ParentRelation(
 {
     /// <summary>
     /// Stores parent relation between two statements using their line numbers.
+    /// Key is a line number of a CHILD node, value is a line number of a PARENT node (child can have only one parent).
     /// </summary>
-    /// <remarks>[parent node line number, child node line number].</remarks>
-    private readonly Dictionary<int, int> _parentsDictionary = new();
+    /// <remarks>[child node line number, parent node line number].</remarks>
+    private readonly Dictionary<int, int> _childParentDictionary = new();
 
     public void SetParent(TreeNode parentNode, TreeNode childNode)
     {
-        if (parentNode.Type.IsContainerStatement() == false || childNode.Type.IsStatement() == false)
+        if (!parentNode.Type.IsContainerStatement())
         {
             logger.LogError(
-                "Parent relation can only be established between container statement and statement node. ({parent} => {child})",
-                parentNode, childNode);
+                "Parent relation can only be established between container statement and statement node. ({parent} must be container statement)",
+                parentNode);
 
             throw new ArgumentException(
-                $"At least one of provided nodes type: {parentNode.Type}, {childNode.Type} is different than any of required: {EntityType.Statement} types.");
+                $"Parent node type {parentNode.Type} is different than any of required {EntityType.Statement} container types.");
         }
 
-        if (!_parentsDictionary.TryAdd(parentNode.LineNumber, childNode.LineNumber))
+        if (!childNode.Type.IsStatement())
+        {
+            logger.LogError(
+                "Parent relation can only be established between container statement and statement node. ({child} must be statement)",
+                childNode);
+
+            throw new ArgumentException(
+                $"Child node type {parentNode.Type} is different than any of required {EntityType.Statement} types.");
+        }
+
+        if (!_childParentDictionary.TryAdd(childNode.LineNumber, parentNode.LineNumber))
         {
             logger.LogError("Relation {parent} parents {child} already exists",
                 parentNode.LineNumber, childNode.LineNumber);
