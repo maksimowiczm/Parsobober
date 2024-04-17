60085e1a481835cdd911d5d9c44bbc88a2203381
bciereszynski <b.ciereszynski@wp.pl>
2024-04-18 00:53:55 +0200

    [BOB-16] add context extractor

---
+++ src/SpaFronted/Parsobober.Simple.Parser/Extractor/ProgramContextExtractor.cs
@@ -0,0 +1,37 @@
+﻿using Parsobober.Pkb.Ast;
+using Parsobober.Pkb.Relations.Abstractions.Creators;
+using Parsobober.Simple.Parser.Abstractions;
+
+namespace Parsobober.Simple.Parser.Extractor
+{
+    internal class ProgramContextExtractor : SimpleExtractor
+    {
+        private readonly IProgramContextCreator creator;
+
+        public ProgramContextExtractor(ISimpleExtractor wrappee, IProgramContextCreator creator) : base(wrappee)
+        {
+            this.creator = creator;
+        }
+
+        public override TreeNode Stmt()
+        {
+            var result = wrappee.Stmt();
+            creator.TryAddStatement(result);
+            return result;
+        }
+
+        public override TreeNode Procedure()
+        {
+            var result = wrappee.Procedure();
+            creator.TryAddProcedure(result);
+            return result;
+        }
+
+        public override TreeNode Variable()
+        {
+            var result = wrappee.Variable();
+            creator.TryAddVariable(result);
+            return result;
+        }
+    }
+}
--- src/SpaFronted/Parsobober.Simple.Parser/SimpleParserBuilder.cs
+++ src/SpaFronted/Parsobober.Simple.Parser/SimpleParserBuilder.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Extensions.Logging;
 using Parsobober.Pkb.Ast.Abstractions;
+using Parsobober.Pkb.Relations.Abstractions;
 using Parsobober.Simple.Lexer;
 using Parsobober.Simple.Parser.Abstractions;
+using Parsobober.Simple.Parser.Extractor;
 
 namespace Parsobober.Simple.Parser;
 
@@ -9,6 +11,7 @@ internal class SimpleParserBuilder(
     ILogger<SimpleParserBuilder> logger,
     IAst ast,
     ILogger<SimpleParser> parserLogger,
+    IPkbCreators creators,
     SlyLexerAdapter lexer
 ) : IParserBuilder
 {
@@ -17,6 +20,6 @@ internal class SimpleParserBuilder(
         logger.LogInformation("Creating simple parser");
         var tokens = lexer.Tokenize(programCode);
         logger.LogInformation("Parsing completed successfully");
-        return new SimpleParser(tokens.GetEnumerator(), ast, parserLogger);
+        return new ProgramContextExtractor(new SimpleParser(tokens.GetEnumerator(), ast, parserLogger), creators.ProgramContext);
     }
 }
