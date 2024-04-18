5b85d01386edd44ba806b814fd007dd41f15fafa
bciereszynski <b.ciereszynski@wp.pl>
2024-04-18 19:57:27 +0200

    [BOB-16] Uses and modifies extractor implement

--- src/SpaFronted/Parsobober.DesignExtractor/ModifiesExtractor.cs
+++ src/SpaFronted/Parsobober.DesignExtractor/ModifiesExtractor.cs
@@ -6,10 +6,25 @@ namespace Parsobober.Simple.Parser.Extractor
     internal class ModifiesExtractor(IModifiesCreator creator) : SimpleExtractor
     {
         private readonly IModifiesCreator creator = creator;
+        private Stack<TreeNode> stack = new Stack<TreeNode>();
+
+        public override void While(TreeNode result)
+        {
+            Stack<TreeNode> tempStack = new Stack<TreeNode>();
+            while (stack.Count > 0)
+            {
+                TreeNode node = stack.Pop();
+                creator.SetModifies(result, node);
+                tempStack.Push(node);
+            }
+            stack = new Stack<TreeNode>(tempStack);
+
+        }
 
         public override void Assign(TreeNode result)
         {
             creator.SetModifies(result, result.Children[0]);
+            stack.Push(result.Children[0]);
         }
     }
 }
--- src/SpaFronted/Parsobober.DesignExtractor/UsesExtractor.cs
+++ src/SpaFronted/Parsobober.DesignExtractor/UsesExtractor.cs
@@ -6,10 +6,42 @@ namespace Parsobober.Simple.Parser.Extractor
     internal class UsesExtractor(IUsesCreator creator) : SimpleExtractor
     {
         private readonly IUsesCreator creator = creator;
+        private Stack<TreeNode> containerStack = new Stack<TreeNode>();
+        private Stack<TreeNode> assignStack = new Stack<TreeNode>();
 
         public override void While(TreeNode result)
         {
+            Stack<TreeNode> tempStack = new Stack<TreeNode>();
+            while (containerStack.Count > 0)
+            {
+                TreeNode node = containerStack.Pop();
+                creator.SetUses(result, node);
+                tempStack.Push(node);
+            }
+            containerStack = new Stack<TreeNode>(tempStack);
+
             creator.SetUses(result, result.Children[0]);
+            containerStack.Push(result.Children[0]);
+        }
+
+        public override void Assign(TreeNode result)
+        {
+            while (assignStack.Count > 0)
+            {
+                TreeNode node = assignStack.Pop();
+                creator.SetUses(result, node);
+                containerStack.Push(node);
+            }
+        }
+
+        public override void Expr(TreeNode result)
+        {
+            assignStack.Push(result.Children[0]);
+
+            if (result.Children[1].Type == EntityType.Variable)
+            {
+                assignStack.Push(result.Children[1]);
+            }
         }
     }
 }
