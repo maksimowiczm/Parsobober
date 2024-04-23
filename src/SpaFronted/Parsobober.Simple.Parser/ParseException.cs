5e757e88b76348ff89a1dd87492ce68a918069c3
bciereszynski <b.ciereszynski@wp.pl>
2024-04-23 23:57:35 +0200

    [BOB-15] remove unused ParseException constructor

--- src/SpaFronted/Parsobober.Simple.Parser/ParseException.cs
+++ src/SpaFronted/Parsobober.Simple.Parser/ParseException.cs
@@ -4,11 +4,6 @@ namespace Parsobober.Simple.Parser
 {
     public class ParseException : Exception
     {
-        public ParseException(LexicalToken getToken, string value, SimpleToken type) : base(string.Format("Parse exception: got {0}:{1}, expected {2}:{3}",
-            getToken.Type, getToken.Value, type, value))
-        {
-        }
-
         public ParseException(LexicalToken getToken, SimpleToken type) :
             base(string.Format("Parse exception: got {0}:{1}, expected {2}",
             getToken.Type, getToken.Value, type))
