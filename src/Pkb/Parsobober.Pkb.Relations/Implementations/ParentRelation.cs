54884b43fdbed1d7be26f60143f3d96830520f33
Klaudia <klaudia134000@wp.pl>
2024-04-23 15:13:09 +0200

    [BOB-18] Implement IParentAccessor

--- src/Pkb/Parsobober.Pkb.Relations/Implementations/ParentRelation.cs
+++ src/Pkb/Parsobober.Pkb.Relations/Implementations/ParentRelation.cs
@@ -2,13 +2,15 @@ using Microsoft.Extensions.Logging;
 using Parsobober.Pkb.Ast;
 using Parsobober.Pkb.Relations.Abstractions.Accessors;
 using Parsobober.Pkb.Relations.Abstractions.Creators;
+using Parsobober.Pkb.Relations.Dto;
+using Parsobober.Pkb.Relations.Utilities;
 
 namespace Parsobober.Pkb.Relations.Implementations;
 
 public class ParentRelation(
     ILogger<ParentRelation> logger,
     IProgramContextAccessor programContext
-) : IParentCreator
+) : IParentCreator, IParentAccessor
 {
     /// <summary>
     /// Stores parent relation between two statements using their line numbers.
@@ -44,5 +46,53 @@ public class ParentRelation(
             logger.LogError("Relation {parent} parents {child} already exists",
                 parentNode.LineNumber, childNode.LineNumber);
         }
+    }
+
+    public IEnumerable<Statement> GetChildren<TParentStatement>() where TParentStatement : Statement
+    {
+        return _childParentDictionary
+           .Where(statement => programContext.StatementsDictionary[statement.Value].IsType<TParentStatement>())
+           .Select(statement => programContext.StatementsDictionary[statement.Key].ToStatement());
+    }
+
+    public IEnumerable<Statement> GetChildren(int lineNumber)
+    {
+        return _childParentDictionary
+            .Where(stmt => stmt.Value == lineNumber)
+            .Select(stmt => programContext.StatementsDictionary[stmt.Key].ToStatement());
+    }
+
+    public Statement? GetParent(int lineNumber)
+    {
+        return _childParentDictionary.TryGetValue(lineNumber, out var statement) ?
+             programContext.StatementsDictionary[statement].ToStatement() :
+             null;
+    }
+
+    public IEnumerable<Statement> GetParents<TChildStatement>() where TChildStatement : Statement
+    {
+        return _childParentDictionary
+            .Where(statement => programContext.StatementsDictionary[statement.Key].IsType<TChildStatement>())
+            .Select(statement => programContext.StatementsDictionary[statement.Value].ToStatement());
+    }
+
+    public IEnumerable<Statement> GetChildrenTransitive<TParentStatement>() where TParentStatement : Statement
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Statement> GetChildrenTransitive(int lineNumber)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Statement> GetParentsTransitive<TChildStatement>() where TChildStatement : Statement
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Statement> GetParentsTransitive(int lineNumber)
+    {
+        throw new NotImplementedException();
     }
 }
