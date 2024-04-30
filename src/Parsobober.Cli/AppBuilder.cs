4a3a74ece8d1b87a4cce2f1bea772dc3c9954071
maksimowiczm <maksimowiczmateusz13@gmail.com>
2024-04-30 11:53:47 +0200

    [BOB-23] Make app testable

---
+++ src/Parsobober.Cli/AppBuilder.cs
@@ -0,0 +1,40 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Parsobober.DesignExtractor;
+using Parsobober.Pkb.Ast;
+using Parsobober.Pkb.Relations;
+using Parsobober.Pql.Parser;
+using Parsobober.Pql.Query;
+using Parsobober.Simple.Lexer;
+using Parsobober.Simple.Parser;
+
+namespace Parsobober.CLI;
+
+public class AppBuilder
+{
+    private readonly HostApplicationBuilder _innerBuilder = Host.CreateApplicationBuilder();
+
+    public AppBuilder()
+    {
+        _innerBuilder.Services
+            .AddSimpleLexer()
+            .AddDesignExtractor()
+            .AddSimpleParserBuilder()
+            .AddAst()
+            .AddRelations()
+            .AddPqlParser();
+    }
+
+    public AppBuilder RemoveLogging()
+    {
+        _innerBuilder.Logging.ClearProviders();
+        return this;
+    }
+
+    public App Build(string code)
+    {
+        var host = _innerBuilder.Build();
+        return new App(host.Services.CreateScope(), code);
+    }
+}
