acbe864ac98863af8e5264391ddab9badaa4b2d6
maksimowiczm <maksimowiczmateusz13@gmail.com>
2024-05-23 11:25:01 +0200

    [BOB-39] PQL parser tests

--- tests/Parsobober.Pql.Parser.Tests/PqlParserTests.cs
+++ tests/Parsobober.Pql.Parser.Tests/PqlParserTests.cs
@@ -187,4 +187,177 @@ public class PqlParserTests
         builderMock.Verify(b => b.AddParent("s1", "10"));
         builderMock.Verify(b => b.Build(), Times.Once);
     }
+
+    [Fact]
+    public void ParseSingleRelationWithMultipleWith()
+    {
+        // Arrange
+        var builderMock = new Mock<IQueryBuilder>();
+        var queryMock = new Mock<IQueryResult>();
+        var parser = new PqlParser(builderMock.Object);
+
+        const string queryString = """
+                                   stmt s1;
+                                   Select s1 such that Parent(s1, 10)
+                                   with s1.stmt# = 5 and s1.value = 10
+                                   """;
+        builderMock.Setup(b => b.Build()).Returns(queryMock.Object);
+
+        // Act
+        var query = parser.Parse(queryString);
+
+        // Assert
+        query.Should().NotBeNull();
+        query.Should().Be(queryMock.Object);
+        builderMock.Verify(b => b.AddDeclaration("stmt s1;"));
+        builderMock.Verify(b => b.AddSelect("s1"));
+        builderMock.Verify(b => b.AddParent("s1", "10"));
+        builderMock.Verify(b => b.With("s1.stmt#", "5"));
+        builderMock.Verify(b => b.With("s1.value", "10"));
+        builderMock.Verify(b => b.Build(), Times.Once);
+    }
+
+    [Fact]
+    public void ParseDoubleRelation()
+    {
+        // Arrange
+        var builderMock = new Mock<IQueryBuilder>();
+        var queryMock = new Mock<IQueryResult>();
+        var parser = new PqlParser(builderMock.Object);
+
+        const string queryString = "stmt s; while w, w1; Select w such that Parent(w, w1) and Parent(w1, s)";
+        builderMock.Setup(b => b.Build()).Returns(queryMock.Object);
+
+        // Act
+        var query = parser.Parse(queryString);
+
+        // Assert
+        query.Should().NotBeNull();
+        query.Should().Be(queryMock.Object);
+        builderMock.Verify(b => b.AddDeclaration("stmt s;"));
+        builderMock.Verify(b => b.AddDeclaration("while w, w1;"));
+        builderMock.Verify(b => b.AddSelect("w"));
+        builderMock.Verify(b => b.AddParent("w", "w1"));
+        builderMock.Verify(b => b.AddParent("w1", "s"));
+        builderMock.Verify(b => b.Build(), Times.Once);
+    }
+
+    [Fact]
+    public void ParseDoubleRelationWith()
+    {
+        // Arrange
+        var builderMock = new Mock<IQueryBuilder>();
+        var queryMock = new Mock<IQueryResult>();
+        var parser = new PqlParser(builderMock.Object);
+
+        const string queryString =
+            "stmt s; while w, w1; Select w such that Parent(w, w1) and Parent(w1, s) with w.stmt# = 5";
+        builderMock.Setup(b => b.Build()).Returns(queryMock.Object);
+
+        // Act
+        var query = parser.Parse(queryString);
+
+        // Assert
+        query.Should().NotBeNull();
+        query.Should().Be(queryMock.Object);
+        builderMock.Verify(b => b.AddDeclaration("stmt s;"));
+        builderMock.Verify(b => b.AddDeclaration("while w, w1;"));
+        builderMock.Verify(b => b.AddSelect("w"));
+        builderMock.Verify(b => b.AddParent("w", "w1"));
+        builderMock.Verify(b => b.AddParent("w1", "s"));
+        builderMock.Verify(b => b.With("w.stmt#", "5"));
+        builderMock.Verify(b => b.Build(), Times.Once);
+    }
+
+    [Fact]
+    public void ParseDoubleRelationWithMultipleWith()
+    {
+        // Arrange
+        var builderMock = new Mock<IQueryBuilder>();
+        var queryMock = new Mock<IQueryResult>();
+        var parser = new PqlParser(builderMock.Object);
+
+        const string queryString = """
+                                   stmt s; while w, w1;
+                                   Select w such that Parent(w, w1) and Parent(w1, s)
+                                   with w.stmt# = 5 and w1.stmt# = 7
+                                   """;
+        builderMock.Setup(b => b.Build()).Returns(queryMock.Object);
+
+        // Act
+        var query = parser.Parse(queryString);
+
+        // Assert
+        query.Should().NotBeNull();
+        query.Should().Be(queryMock.Object);
+        builderMock.Verify(b => b.AddDeclaration("stmt s;"));
+        builderMock.Verify(b => b.AddDeclaration("while w, w1;"));
+        builderMock.Verify(b => b.AddSelect("w"));
+        builderMock.Verify(b => b.AddParent("w", "w1"));
+        builderMock.Verify(b => b.AddParent("w1", "s"));
+        builderMock.Verify(b => b.With("w.stmt#", "5"));
+        builderMock.Verify(b => b.With("w1.stmt#", "7"));
+        builderMock.Verify(b => b.Build(), Times.Once);
+    }
+
+    [Fact]
+    public void ParseTripleRelation()
+    {
+        // Arrange
+        var builderMock = new Mock<IQueryBuilder>();
+        var queryMock = new Mock<IQueryResult>();
+        var parser = new PqlParser(builderMock.Object);
+
+        const string queryString =
+            "stmt s; while w, w1, w2; Select w such that Parent(w, w1) and Parent(w1, w2) and Parent(w2, s)";
+        builderMock.Setup(b => b.Build()).Returns(queryMock.Object);
+
+        // Act
+        var query = parser.Parse(queryString);
+
+        // Assert
+        query.Should().NotBeNull();
+        query.Should().Be(queryMock.Object);
+        builderMock.Verify(b => b.AddDeclaration("stmt s;"));
+        builderMock.Verify(b => b.AddDeclaration("while w, w1, w2;"));
+        builderMock.Verify(b => b.AddSelect("w"));
+        builderMock.Verify(b => b.AddParent("w", "w1"));
+        builderMock.Verify(b => b.AddParent("w1", "w2"));
+        builderMock.Verify(b => b.AddParent("w2", "s"));
+        builderMock.Verify(b => b.Build(), Times.Once);
+    }
+
+    [Fact]
+    public void ParseTripleRelationWithMultipleWith()
+    {
+        // Arrange
+        var builderMock = new Mock<IQueryBuilder>();
+        var queryMock = new Mock<IQueryResult>();
+        var parser = new PqlParser(builderMock.Object);
+
+        const string queryString = """
+                                   stmt s; while w, w1, w2;
+                                   Select w such that Parent(w, w1) and Parent(w1, w2) and Parent(w2, s)
+                                   with w.stmt# = 5 and w1.stmt# = 7 and w2.stmt# = 10
+                                   """;
+
+        builderMock.Setup(b => b.Build()).Returns(queryMock.Object);
+
+        // Act
+        var query = parser.Parse(queryString);
+
+        // Assert
+        query.Should().NotBeNull();
+        query.Should().Be(queryMock.Object);
+        builderMock.Verify(b => b.AddDeclaration("stmt s;"));
+        builderMock.Verify(b => b.AddDeclaration("while w, w1, w2;"));
+        builderMock.Verify(b => b.AddSelect("w"));
+        builderMock.Verify(b => b.AddParent("w", "w1"));
+        builderMock.Verify(b => b.AddParent("w1", "w2"));
+        builderMock.Verify(b => b.AddParent("w2", "s"));
+        builderMock.Verify(b => b.With("w.stmt#", "5"));
+        builderMock.Verify(b => b.With("w1.stmt#", "7"));
+        builderMock.Verify(b => b.With("w2.stmt#", "10"));
+        builderMock.Verify(b => b.Build(), Times.Once);
+    }
 }
