2a153673c34d8ba2dc9d248663aff3c215a25822
maksimowiczm <maksimowiczmateusz13@gmail.com>
2024-05-26 14:10:08 +0200

    [BOB-40] Add `DependentQueryNode` documentation 💀

--- src/QueryProcessor/Parsobober.Pql.Query/Tree/Node/DependentQueryNode.cs
+++ src/QueryProcessor/Parsobober.Pql.Query/Tree/Node/DependentQueryNode.cs
@@ -1,3 +1,4 @@
+using Parsobober.Pkb.Relations.Dto;
 using Parsobober.Pql.Query.Arguments;
 using Parsobober.Pql.Query.Queries.Abstractions;
 
@@ -5,15 +6,32 @@ namespace Parsobober.Pql.Query.Tree.Node;
 
 internal class DependentQueryNode : IQueryNode
 {
-    private readonly IQueryDeclaration _conditionQuery;
-    private readonly IDeclaration _conditionSelect;
-    private readonly IQueryNode _dependent;
+    private readonly IQueryDeclaration _queryToReplace;
+    private readonly IDeclaration _argumentToReplace;
+    private readonly IQueryNode _replacerQueryNode;
 
-    public DependentQueryNode(IQueryDeclaration conditionQuery, IDeclaration conditionSelect, IQueryNode dependent)
+    /// <summary>
+    /// Creates new instance of <see cref="DependentQueryNode"/>.
+    /// Replaces <paramref name="argumentToReplace"/> argument with results from <paramref name="replacerQueryNode"/>.
+    /// </summary>
+    /// <param name="queryToReplace">
+    /// Query that will be executed for each result of <paramref name="replacerQueryNode"/>.
+    /// </param>
+    /// <param name="argumentToReplace">
+    /// Select statement that will be replaced with results from <paramref name="replacerQueryNode"/>.
+    /// </param>
+    /// <param name="replacerQueryNode">
+    /// Query that will be executed to get results for <paramref name="argumentToReplace"/> replacement.
+    /// </param>
+    public DependentQueryNode(
+        IQueryDeclaration queryToReplace,
+        IDeclaration argumentToReplace,
+        IQueryNode replacerQueryNode
+    )
     {
-        _conditionQuery = conditionQuery;
-        _conditionSelect = conditionSelect;
-        _dependent = dependent;
+        _queryToReplace = queryToReplace;
+        _argumentToReplace = argumentToReplace;
+        _replacerQueryNode = replacerQueryNode;
     }
 
     public IEnumerable<IComparable> Do()
@@ -21,16 +39,18 @@ internal class DependentQueryNode : IQueryNode
         // god forgive me
         // I am doing this only to pass my studies
 
-        var dependentResult = _dependent.Do().Select(r =>
-            // 💀💀💀💀💀💀💀💀💀
-            IArgument.Parse(new Dictionary<string, IDeclaration>(), r.ToString()!)
-        );
+        var dependentResult = _replacerQueryNode
+            .Do()
+            .Select(r =>
+                // 💀💀💀💀💀💀💀💀💀
+                IArgument.Parse(new Dictionary<string, IDeclaration>(), r.ToString()!)
+            );
 
         var result = dependentResult
             .SelectMany(arg =>
-                _conditionQuery
-                    .ReplaceArgument(_conditionSelect, arg)
-                    .Do(_conditionSelect)
+                _queryToReplace
+                    .ReplaceArgument(_argumentToReplace, arg)
+                    .Do(_argumentToReplace)
             )
             .Distinct();
 
