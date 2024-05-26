1a4cf4c306e951ce8fa5dad2bd945e81a279b87b
pzajaczkowski <demoon.pps@gmail.com>
2024-05-26 21:41:12 +0200

    [BOB-420] Fix DoubleRelationTest

--- tests/Parsobober.Tests/Sprint2/DoubleRelationTests.cs
+++ tests/Parsobober.Tests/Sprint2/DoubleRelationTests.cs
@@ -6,7 +6,7 @@ public class DoubleRelationTests() : BaseTestClass(Code.ZadanieDomowe1)
     // Mateusz
     [InlineData("stmt s; if i; while w; Select i such that Parent(i, w) and Parent(w, s)", "8")]
     [InlineData("stmt s; if i; while w; Select w such that Parent(i, w) and Parent(w, s)", "10")]
-    [InlineData("stmt s; if i; while w; Select s such that Parent(i, w) and Parent(w, s)", "11,12,13,14,15,16")]
+    [InlineData("stmt s; if i; while w; Select s such that Parent(i, w) and Parent(w, s)", "11,12")]
     [InlineData("stmt s; while w; assign a; Select w such that Parent(w, s) and Parent(s, a)", "23")]
     public void DoubleRelationTest(string query, string expected)
     {
