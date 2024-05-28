357f9a220f0926f5874785c14bf351ec41ae5304
maksimowiczm <maksimowiczmateusz13@gmail.com>
2024-05-28 15:35:46 +0200

    [BOB-42] Add boolean queries tests

---
+++ tests/Parsobober.Tests/Sprint2/BooleanTests.cs
@@ -0,0 +1,51 @@
+namespace Parsobober.Tests.Sprint2;
+
+public class BooleanTests() : BaseTestClass(Code.ZadanieDomowe1)
+{
+    [Theory]
+    // Parent
+    [InlineData("""
+                stmt s, s1;
+                Select BOOLEAN such that Parent(s, s1)
+                """, true)]
+    [InlineData("""
+                stmt s; assign a;
+                Select BOOLEAN such that Parent(a, s)
+                """, false)]
+    [InlineData("Select BOOLEAN such that Parent(1, 2)", false)]
+    [InlineData("Select BOOLEAN such that Parent(8, 9)", true)]
+    [InlineData("Select BOOLEAN such that Parent(8, 11)", false)]
+    [InlineData("Select BOOLEAN such that Parent*(8, 11)", true)]
+    // Follows
+    [InlineData("Select BOOLEAN such that Follows(1, 2)", true)]
+    [InlineData("Select BOOLEAN such that Follows(2, 1)", false)]
+    [InlineData("Select BOOLEAN such that Follows(1, 4)", false)]
+    [InlineData("Select BOOLEAN such that Follows*(1, 4)", true)]
+    // Modifies
+    [InlineData("""
+                Select BOOLEAN such that Modifies(1, "x")
+                """, false)]
+    [InlineData("""
+                Select BOOLEAN such that Modifies(1, "t")
+                """, true)]
+    // Uses
+    [InlineData("""
+                Select BOOLEAN such that Uses(2, "x")
+                """, false)]
+    [InlineData("""
+                Select BOOLEAN such that Uses(2, "t")
+                """, true)]
+    [InlineData("""
+                stmt s; assign a;
+                Select BOOLEAN such that Parent(s, a) with s.stmt# = 1 and a.stmt# = 2
+                """, false)]
+    [InlineData("""
+                stmt s; assign a;
+                Select BOOLEAN such that Parent(s, a) with s.stmt# = 8 and a.stmt# = 9
+                """, true)]
+    public void BooleanTest(string query, bool expected)
+    {
+        var result = App.Query(query);
+        result.Should().Be(expected ? "TRUE" : "FALSE");
+    }
+}
