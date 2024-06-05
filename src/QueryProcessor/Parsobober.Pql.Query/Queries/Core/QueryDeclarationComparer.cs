a6af70f1b0e847aa4db3ce12cb99f48b026d0edd
maksimowiczm <maksimowiczmateusz13@gmail.com>
2024-06-05 12:26:05 +0200

    [BOB-52] Create `QueryDeclarationComparer` template

--- src/QueryProcessor/Parsobober.Pql.Query/Queries/Core/QueryDeclarationComparer.cs
+++ src/QueryProcessor/Parsobober.Pql.Query/Queries/Core/QueryDeclarationComparer.cs
@@ -1,3 +1,4 @@
+using Parsobober.Pql.Query.Arguments;
 using Parsobober.Pql.Query.Queries.Abstractions;
 
 namespace Parsobober.Pql.Query.Queries.Core;
@@ -6,6 +7,39 @@ internal class QueryDeclarationComparer : IComparer<IQueryDeclaration>
 {
     public int Compare(IQueryDeclaration? x, IQueryDeclaration? y)
     {
-        return 1;
+        if (x == null || y == null)
+            return 0;
+
+        var xRating = RateQuery(x);
+        var yRating = RateQuery(y);
+
+        return xRating - yRating;
     }
+
+    /// <summary>
+    /// Returns a rating for the query. The higher the rating, the more complex the query is.
+    /// </summary>
+    private static int RateQuery(IQueryDeclaration query) =>
+        query switch
+        {
+            { Left: not IDeclaration, Right: not IDeclaration } => RateTwoArguments(query),
+            { Left: IDeclaration, Right: not IDeclaration } or
+                { Left: not IDeclaration, Right: IDeclaration } => RateTwoDeclarations(query),
+            _ => RateTwoArguments(query),
+        };
+
+    /// <summary>
+    /// Rates a query with two arguments, example Parent(1, 2).
+    /// </summary>
+    private static int RateTwoArguments(IQueryDeclaration query) => 0;
+
+    /// <summary>
+    /// Rates a query with one argument, example Parent(s, 1).
+    /// </summary>
+    private static int RateOneArgument(IQueryDeclaration query) => 0;
+
+    /// <summary>
+    /// Rates a query with two declarations, example Parent(s, a).
+    /// </summary>
+    private static int RateTwoDeclarations(IQueryDeclaration query) => 0;
 }
