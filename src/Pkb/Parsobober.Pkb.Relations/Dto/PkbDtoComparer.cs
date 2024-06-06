be8af01fc9dc0d6d10a0ba10583e88d7e7b0da02
maksimowiczm <maksimowiczmateusz13@gmail.com>
2024-06-06 12:45:29 +0200

    [BOB-54] Fix `prog_line` queries

--- src/Pkb/Parsobober.Pkb.Relations/Dto/PkbDtoComparer.cs
+++ src/Pkb/Parsobober.Pkb.Relations/Dto/PkbDtoComparer.cs
@@ -4,49 +4,51 @@ namespace Parsobober.Pkb.Relations.Dto;
 
 public class PkbDtoComparer : IEqualityComparer<IPkbDto>, IComparer<IPkbDto>
 {
-    public bool Equals(IPkbDto? x, IPkbDto? y)
+    // equal
+    public bool Equals(IPkbDto? xdto, IPkbDto? ydto)
     {
-        if (x is null || y is null)
+        if (xdto is null || ydto is null)
             return false;
 
-        var result = (x, y) switch
+        var result = (xdto, ydto) switch
         {
-            (Statement xStatement, Statement yStatement) => xStatement.Line == yStatement.Line,
-            (Statement xStatement, ProgramLine yProgramLine) => xStatement.Line == yProgramLine.Line,
-            (ProgramLine xProgramLine, Statement yStatement) => xProgramLine.Line == yStatement.Line,
-            (Variable xVariable, Variable yVariable) => xVariable.Name == yVariable.Name,
-            (Procedure xProcedure, Procedure yProcedure) => xProcedure.Name == yProcedure.Name,
+            (Statement x, Statement y) => x.Line == y.Line,
+            (Statement x, ProgramLine y) => x.Line == y.Line,
+            (ProgramLine x, Statement y) => x.Line == y.Line,
+            (Variable x, Variable y) => x.Name == y.Name,
+            (Procedure x, Procedure y) => x.Name == y.Name,
             _ => false
         };
 
         return result;
     }
 
-    public int GetHashCode(IPkbDto obj) => obj.GetHashCode();
+    public int GetHashCode(IPkbDto obj) => 0;
 
-    public int Compare(IPkbDto? x, IPkbDto? y)
+    // order
+    public int Compare(IPkbDto? xdto, IPkbDto? ydto)
     {
-        if (x is null || y is null)
+        if (xdto is null || ydto is null)
             return 0;
 
-        var result = (x, y) switch
+        var result = (xdto, ydto) switch
         {
-            (Statement xStatement, Statement yStatement) =>
-                xStatement.Line.CompareTo(yStatement.Line),
-            (Statement xStatement, ProgramLine yProgramLine) =>
-                xStatement.Line.CompareTo(yProgramLine.Line),
-            (ProgramLine xProgramLine, Statement yStatement) =>
-                xProgramLine.Line.CompareTo(yStatement.Line),
-            (Variable xVariable, Variable yVariable) =>
-                string.Compare(xVariable.Name, yVariable.Name, StringComparison.Ordinal),
-            (Procedure xProcedure, Procedure yProcedure) =>
-                string.Compare(xProcedure.Name, yProcedure.Name, StringComparison.Ordinal),
-            (Constant xConstant, Constant yConstant) =>
-                xConstant.Value.CompareTo(yConstant.Value),
-            (ProgramLine xProgramLine, ProgramLine yProgramLine) =>
-                xProgramLine.Line.CompareTo(yProgramLine.Line),
-            (StatementList xStatementList, StatementList yStatementList) =>
-                xStatementList.Line.CompareTo(yStatementList.Line),
+            (Statement x, Statement y) =>
+                x.Line.CompareTo(y.Line),
+            (Statement x, ProgramLine y) =>
+                x.Line.CompareTo(y.Line),
+            (ProgramLine x, Statement y) =>
+                x.Line.CompareTo(y.Line),
+            (Variable x, Variable y) =>
+                string.Compare(x.Name, y.Name, StringComparison.Ordinal),
+            (Procedure x, Procedure y) =>
+                string.Compare(x.Name, y.Name, StringComparison.Ordinal),
+            (Constant x, Constant y) =>
+                x.Value.CompareTo(y.Value),
+            (ProgramLine x, ProgramLine y) =>
+                x.Line.CompareTo(y.Line),
+            (StatementList x, StatementList y) =>
+                x.Line.CompareTo(y.Line),
             _ => 0
         };
 
