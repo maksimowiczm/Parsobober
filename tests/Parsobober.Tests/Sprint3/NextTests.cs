f653227413b1ca5e17fe4685f198b48262c35efb
maksimowiczm <maksimowiczmateusz13@gmail.com>
2024-06-07 08:37:23 +0200

    [BOB-56] Add `Next` tests

---
+++ tests/Parsobober.Tests/Sprint3/NextTests.cs
@@ -0,0 +1,15 @@
+namespace Parsobober.Tests.Sprint3;
+
+public class NextTests() : BaseTestClass(Code.ZadanieDomowe1)
+{
+    [Theory]
+    [InlineData("prog_line pl, pl1;Select pl such that Next(pl, pl1)",
+        "1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 10, 11, 14, 15, 13, 16, 21, 18, 19, 20, 25, 26, 23, 24")]
+    [InlineData("prog_line pl, pl1;Select pl1 such that Next(pl, pl1)",
+        "2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27")]
+    public void NextTest(string query, string expected)
+    {
+        var result = App.Query(query);
+        result.Should().Be(expected.Replace(" ", ""));
+    }
+}
