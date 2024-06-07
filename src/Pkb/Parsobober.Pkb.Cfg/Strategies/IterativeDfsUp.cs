15d5cf4c1d4dabf186725200d288a754f1e3821a
pzajaczkowski <demoon.pps@gmail.com>
2024-06-07 15:04:58 +0200

    [BOB-56] Add Cfg project

---
+++ src/Pkb/Parsobober.Pkb.Cfg/Strategies/IterativeDfsUp.cs
@@ -0,0 +1,37 @@
+﻿using Parsobober.Pkb.Cfg.Abstractions;
+
+namespace Parsobober.Pkb.Cfg.Strategies;
+
+public class IterativeDfsUp : ICfgTraversalStrategy
+{
+    public IEnumerable<ICfgNode> Traverse(ICfgNode startNode)
+    {
+        var visited = new HashSet<ICfgNode>();
+        var stack = new Stack<ICfgNode>();
+        var result = new List<ICfgNode>();
+
+        stack.Push(startNode);
+
+        while (stack.Count > 0)
+        {
+            var node = stack.Pop();
+
+            if (!visited.Add(node))
+            {
+                continue;
+            }
+
+            result.Add(node);
+
+            foreach (var previousNode in node.Previous)
+            {
+                if (!visited.Contains(previousNode))
+                {
+                    stack.Push(previousNode);
+                }
+            }
+        }
+
+        return result;
+    }
+}
