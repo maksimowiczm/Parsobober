5eb23a141c75ce423ba34bf72bdc560bf9c165a1
bciereszynski <b.ciereszynski@wp.pl>
2024-06-09 17:24:25 +0200

    BOB-53 add basic pattern tests

--- tests/Parsobober.Tests/Sprint3/PatternTests.cs
+++ tests/Parsobober.Tests/Sprint3/PatternTests.cs
@@ -4,6 +4,11 @@ public class PatternTests() : BaseTestClass(Code.Dropbox)
 {
     [Theory]
     [InlineData("assign a;\nSelect a pattern a(_, \"width + incre + left\")", "7")]
+    [InlineData("assign a;\nSelect a pattern a(_, \"length + x1\")", "7")]
+    [InlineData("assign a;\nSelect a pattern a(_, _\"y2 - y1\"_)", "50,141,168,249")]
+    [InlineData("assign a;\nSelect a pattern a(_, \"volume * 11 + volume - x9 + volume\")", "58")]
+    [InlineData("assign a;\nSelect a pattern a(_, \"I - (k + j * decrement)\")", "104")]
+    [InlineData("assign a;\nSelect a pattern a( \"x\",_)", "106, 116")]
     public void PatternTest(string query, string expected)
     {
         var result = App.Query(query);
