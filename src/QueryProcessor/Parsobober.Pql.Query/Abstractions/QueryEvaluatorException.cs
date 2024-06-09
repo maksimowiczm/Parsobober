899d3b7c09a492e7072c0605c84dcf36722ce6cc
maksimowiczm <maksimowiczmateusz13@gmail.com>
2024-06-09 18:32:42 +0200

    [BOB-420] Add `WtfException`

--- src/QueryProcessor/Parsobober.Pql.Query/Abstractions/QueryEvaluatorException.cs
+++ src/QueryProcessor/Parsobober.Pql.Query/Abstractions/QueryEvaluatorException.cs
@@ -1,3 +1,5 @@
 namespace Parsobober.Pql.Query.Abstractions;
 
 public abstract class QueryEvaluatorException(string message) : Exception(message);
+
+internal class WtfException(string message) : QueryEvaluatorException(message);
